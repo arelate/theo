ecdd491bff1f12923277e26e5e046224e5a32d25
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-19 13:02:16 -0800

    Implementing (known) custom commands support, and so far postinstall only contained cat commands

--- cli/finalize_installation.go
+++ cli/finalize_installation.go
@@ -1,14 +1,24 @@
 package cli
 
 import (
+	"errors"
 	"github.com/arelate/theo/data"
 	"github.com/arelate/vangogh_local_data"
 	"github.com/boggydigital/kevlar"
 	"github.com/boggydigital/nod"
 	"github.com/boggydigital/pathways"
+	"golang.org/x/exp/slices"
+	"io"
 	"net/url"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	pkgPathPfx = "pkgpath=$(dirname \"$PACKAGE_PATH\")"
+	catCmdPfx  = "cat "
 )
 
 func FinalizeInstallationHandler(u *url.URL) error {
@@ -79,11 +89,22 @@ func finalizeProductInstallation(id, title string,
 
 	productExtractsDir := filepath.Join(extractsDir, id)
 
+	downloadsDir, err := pathways.GetAbsDir(data.Downloads)
+	if err != nil {
+		return fpia.EndWithError(err)
+	}
+
+	productDownloadsDir := filepath.Join(downloadsDir, id)
+
 	for _, link := range links {
 
 		linkOs := vangogh_local_data.ParseOperatingSystem(link.OS)
 		if linkOs != vangogh_local_data.MacOS {
 			// currently only macOS finalization is supported (required?)
+			continue
+		}
+		if filepath.Ext(link.LocalFilename) != pkgExt {
+			// for macOS - there's no need to finalize additional files (not .pkg installers)
 			continue
 		}
 
@@ -101,9 +122,15 @@ func finalizeProductInstallation(id, title string,
 			return fpia.EndWithError(err)
 		}
 
-		bundlePath := filepath.Join(installationDir, bundleName)
+		bundleInstallPath := filepath.Join(installationDir, bundleName)
 
-		if err := removeXattrs(bundlePath); err != nil {
+		if customCommands := pis.CustomCommands(); len(customCommands) > 0 {
+			if err := processCustomCommands(customCommands, productDownloadsDir, bundleInstallPath); err != nil {
+				return fpia.EndWithError(err)
+			}
+		}
+
+		if err := removeXattrs(bundleInstallPath); err != nil {
 			return fpia.EndWithError(err)
 		}
 	}
@@ -118,6 +145,91 @@ func removeXattrs(path string) error {
 	cmd := exec.Command("xattr", "-c", "-r", path)
 	if err := cmd.Run(); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func processCustomCommands(commands []string, productDownloadsDir, bundleInstallPath string) error {
+
+	pcca := nod.NewProgress(" processing custom commands...")
+	defer pcca.EndWithResult("done")
+
+	pcca.TotalInt(len(commands))
+
+	for _, cmd := range commands {
+		if strings.HasPrefix(cmd, pkgPathPfx) {
+			// do nothing, we'll be using downloads location as ${pkgpath}
+			pcca.Increment()
+			continue
+		}
+		if strings.HasPrefix(cmd, catCmdPfx) {
+			if catCmdParts := strings.Split(strings.TrimPrefix(cmd, catCmdPfx), " "); len(catCmdParts) == 3 {
+				srcGlob := strings.Trim(strings.Replace(catCmdParts[0], "\"${pkgpath}\"", productDownloadsDir, 1), "\"")
+				dstPath := strings.Trim(strings.Replace(catCmdParts[2], "${gog_full_path}", bundleInstallPath, 1), "\"")
+				if err := execCatFiles(srcGlob, dstPath); err != nil {
+					return pcca.EndWithError(err)
+				}
+			}
+			pcca.Increment()
+			continue
+		}
+		// at this point we've handled all known commands, so anything here would be unknown
+		return pcca.EndWithError(errors.New("cannot process unknown custom command: " + cmd))
+	}
+	return nil
+}
+
+func execCatFiles(srcGlob string, dstPath string) error {
+
+	if srcGlob == "" {
+		return errors.New("cat command source glob cannot be empty")
+	}
+	if dstPath == "" {
+		return errors.New("cat command destination path cannot be empty")
+	}
+
+	_, srcFileGlob := filepath.Split(srcGlob)
+
+	ecfa := nod.NewProgress(" cat %s into %s...", srcFileGlob, dstPath)
+	defer ecfa.EndWithResult("done")
+
+	dstDir, _ := filepath.Split(dstPath)
+	if _, err := os.Stat(dstDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dstDir, 0755); err != nil {
+			return ecfa.EndWithError(err)
+		}
+	}
+
+	dstFile, err := os.Create(dstPath)
+	if err != nil {
+		return ecfa.EndWithError(err)
+	}
+	defer dstFile.Close()
+
+	matches, err := filepath.Glob(srcGlob)
+	if err != nil {
+		return ecfa.EndWithError(err)
+	}
+
+	slices.Sort(matches)
+
+	ecfa.TotalInt(len(matches))
+
+	for _, match := range matches {
+
+		srcFile, err := os.Open(match)
+		if err != nil {
+			return ecfa.EndWithError(err)
+		}
+
+		if _, err := io.Copy(dstFile, srcFile); err != nil {
+			_ = srcFile.Close()
+			return ecfa.EndWithError(err)
+		}
+
+		ecfa.Increment()
+		_ = srcFile.Close()
 	}
 
 	return nil
