f0680d017756f12e73d12ea7763c02cbd8e43c30
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-20 11:06:41 -0800

    Changing intro text

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	nod.EnableStdOutPresenter()
 
-	ns := nod.Begin("theo is improving your vangogh experience")
+	ns := nod.Begin("theo is complementing vangogh experience")
 	defer ns.End()
 
 	theoRootDir, err := data.InitRootDir()
